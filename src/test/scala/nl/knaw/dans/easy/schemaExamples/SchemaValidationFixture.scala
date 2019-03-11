35a92ea4249db96f000a7c1c94bf3c7cd83409f1
Jo Pol <jo-pol@users.noreply.github.com>
2019-03-11 12:05:53 +0100

    easy schemas loaded with local locations; refactoring

--- src/test/scala/nl/knaw/dans/easy/schemaExamples/SchemaValidationFixture.scala
+++ src/test/scala/nl/knaw/dans/easy/schemaExamples/SchemaValidationFixture.scala
@@ -17,7 +17,7 @@ package nl.knaw.dans.easy.schemaExamples
 
 import java.net.UnknownHostException
 
-import better.files.StringOps
+import better.files.{ File, StringOps }
 import javax.xml.XMLConstants
 import javax.xml.transform.Source
 import javax.xml.transform.stream.StreamSource
@@ -25,47 +25,80 @@ import javax.xml.validation.{ Schema, SchemaFactory }
 import org.scalatest.{ FlatSpec, Matchers }
 
 import scala.util.{ Failure, Try }
-import scala.xml.{ Elem, SAXParseException }
+import scala.xml.{ Elem, SAXParseException, XML }
 
 trait SchemaValidationFixture extends FlatSpec with Matchers {
 
   val schemaFile: String
+
+  private val publicEasySchema = """//easy.dans.knaw.nl/schemas"""
+  private val httpsEasySchema = s"https:$publicEasySchema"
+
   private lazy val triedSchema: Try[Schema] = Try {
     // lazy for two reasons:
     // - schemaFile is set by concrete test class
     // - postpone loading until actually validating
+    val xsdInputStream = File(schemaFile).contentAsString.replaceAll(
+      s"""schemaLocation="http:$publicEasySchema""",
+      s"""schemaLocation="file://$schemaDir"""
+    ).inputStream
     SchemaFactory
       .newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI)
-      .newSchema(Array(new StreamSource(schemaFile)).toArray[Source])
+      .newSchema(Array(new StreamSource(xsdInputStream)).toArray[Source])
   }
 
-  def validate(elem: Elem): Try[Unit] = {
-    assume(triedSchema match {
-      case Failure(e: SAXParseException) if e.getCause != null && e.getCause.isInstanceOf[UnknownHostException] => false
-      case Failure(e: SAXParseException) if e.getMessage.contains("Cannot resolve") =>
-        println("Probably an offline third party schema: " + e.getMessage)
-        false
-      case _ => true
-    })
-    val xmlString = elem
-      .toString() // schema location attribute becomes a standardized one liner
-      .replaceAll(
-      " https://easy.dans.knaw.nl/schemas", // public xsd location
-      s" file://$schemaDir" // local xsd location
-    )
+  def validate(xml: Elem): Try[Unit] = {
+    assume(schemasAreOnline)
+    val xmlString = toLocalSchemas(xml)
     triedSchema.map(_.newValidator().validate(new StreamSource(xmlString.inputStream))) match {
       case Failure(e: SAXParseException) =>
-        val lines = xmlString.split("\n")
-        val lineNumber = e.getLineNumber
-        printNonEmpty(lines.slice(0, lineNumber))
-        println("-" * e.getColumnNumber + "^  " + e.getMessage)
-        printNonEmpty(lines.slice(lineNumber + 1, Int.MaxValue))
+        showErrorWithSourceContext(xmlString, e)
         Failure(e)
       case x => x
     }
   }
 
+  def loadExampleXml(example: String): Elem = {
+    XML.loadFile((exampleDir / example).toString())
+  }
+
+  def locationsIn(xml: Elem): Seq[String] = {
+    Seq(
+      "xsi:schemaLocation",
+      "xsi:noNamespaceSchemaLocation"
+    ).flatMap(xml.attributes.asAttrMap.getOrElse(_, "").split(" +"))
+      .filter(_.endsWith(".xsd"))
+      .map(_.replace(httpsEasySchema, ""))
+  }
+
+  private def schemasAreOnline = {
+    triedSchema match {
+      case Failure(e: SAXParseException) if e.getCause != null && e.getCause.isInstanceOf[UnknownHostException] => false
+      case Failure(e: SAXParseException) if e.getMessage.contains("Cannot resolve") =>
+        println("Probably an offline third party schema: " + e.getMessage)
+        false
+      case _ => true
+    }
+  }
+
+  private def showErrorWithSourceContext(xmlString: String, e: SAXParseException): Unit = {
+    val lines = xmlString.split("\n")
+    val lineNumber = e.getLineNumber
+    printNonEmpty(lines.slice(0, lineNumber))
+    println("-" * e.getColumnNumber + "^  " + e.getMessage)
+    printNonEmpty(lines.slice(lineNumber + 1, Int.MaxValue))
+  }
+
   private def printNonEmpty(lines: Seq[String]): Unit = {
     lines.withFilter(_.trim.nonEmpty).foreach(println)
+  }
+
+  private def toLocalSchemas(xml: Elem) = {
+    xml
+      .toString() // schema location attribute becomes a standardized one liner
+      .replaceAll( // a leading space is supposed to be the location
+      s" $httpsEasySchema", // replace public xsd location
+      s" file://$schemaDir" // with local xsd location
+    )
   }
 }
--- src/test/scala/nl/knaw/dans/easy/schemaExamples/package.scala
+++ src/test/scala/nl/knaw/dans/easy/schemaExamples/package.scala
@@ -1,62 +1,22 @@
-/**
- * Copyright (C) 2019 DANS - Data Archiving and Networked Services (info@dans.knaw.nl)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package nl.knaw.dans.easy
 
 import better.files.File
 
-import scala.util.{ Failure, Try }
-import scala.xml.{ Elem, SAXParseException, XML }
-
 package object schemaExamples {
 
-  val distDir = File("src/main/assembly/dist")
   val schemaDir = File("target/easy-schema")
+  val exampleDir = File("src/main/assembly/dist/examples")
 
   def lastLocalXsd(dir: String, file: String): String = {
-    // this way digits sort after letters
     (schemaDir / dir)
       .walk()
       .map(_.toString())
       .filter(_.matches(".*/[0-9/]{5,}+" + file)) // end with: /YYYY/xxx or /YYYY/MM/xxx
-      .maxBy(_.toUpperCase.map(c => digitsToLowerCase(c)))
-  }
-
-  def masterXsd(dir: String, file: String): String = {
-    (schemaDir / dir / file).toString()
+      .maxBy(_.toUpperCase.map(c => digitsToLowerCase(c))) // digits sort after letters
   }
 
   private def digitsToLowerCase(c: Char): Char = {
     if (c.isDigit) ('a' + c).toChar
     else c
-  }
-
-  def loadExampleXml(example: String): Elem = {
-    XML.loadFile((distDir / "examples" / example).toString())
-  }
-
-  def locationsIn(xml: Elem): Seq[String] = {
-    Seq(
-      "xsi:schemaLocation",
-      "xsi:noNamespaceSchemaLocation"
-    ).flatMap(xml.attributes.asAttrMap.getOrElse(_, "").split(" +"))
-      .filter(_.endsWith(".xsd"))
-      .map(_.replace("https://easy.dans.knaw.nl/schemas", ""))
-  }
-
-  implicit class StringExtensions[T](val s: String) extends AnyVal {
-    def relativeToDistDir: String = s.replace(schemaDir.path.toAbsolutePath.toString, "")
   }
 }
